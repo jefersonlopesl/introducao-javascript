c118cc1e326803e6315ced85e27d7dea6c403627
jefersonlopesl <jefersonlopesluis@gmail.com>
2020-12-15 11:10:16 -0300

    Emplementando um codigo mais monomorfico.

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 
 var peso = tdPeso.textContent;
@@ -7,6 +7,28 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 
-var imc = peso/altura*altura;
+var tdImc = paciente.querySelector(".info-imc");
 
-console.log();
+var pesoEhValido = true;
+
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000) {
+  console.log("Erro de digitação no peso");
+  pesoEhValido = false;
+  tdImc.textContent = "Peso invalido";
+}
+
+if(altura <= 0 || altura >= 3.0) {
+    console.log("Erro de digitação em altura");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura invalida"
+}
+
+if(alturaEhValida && pesoEhValido) {
+
+var imc = peso/ (altura *altura);
+
+tdImc.textContent = imc;
+
+}
