baf1a2382ed7d1e195bbc03afdcc46b2182c685a
jefersonlopesl <jefersonlopesluis@gmail.com>
2020-12-15 16:52:23 -0300

    Editei mais umas linhas

--- js/principal.js
+++ js/principal.js
@@ -1,34 +1,43 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutrição";
+
 var pacientes = document.querySelectorAll(".paciente");
-var tdPeso = paciente.querySelector(".info-peso");
 
-var peso = tdPeso.textContent;
+for(var i= 0; i< pacientes.length; i++) {
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
 
-var tdImc = paciente.querySelector(".info-imc");
+  var tdImc = paciente.querySelector(".info-imc");
 
-var pesoEhValido = true;
+  var pesoEhValido = true;
 
-var alturaEhValida = true;
+  var alturaEhValida = true;
 
-if(peso <= 0 || peso >= 1000) {
-  console.log("Erro de digitação no peso");
-  pesoEhValido = false;
-  tdImc.textContent = "Peso invalido";
-}
+  if(peso <= 0 || peso >= 1000) {
+    console.log("Erro de digitação no peso");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso invalido";
+    paciente.classList.add("paciente-invalido");
+  }
+  if(altura <= 0 || altura >= 3.00) {
+      console.log("Erro de digitação em altura");
+      alturaEhValida = false;
+      tdImc.textContent = "Altura invalida"
+  }
 
-if(altura <= 0 || altura >= 3.0) {
-    console.log("Erro de digitação em altura");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura invalida"
-}
+  if(alturaEhValida && pesoEhValido) {
 
-if(alturaEhValida && pesoEhValido) {
+  var imc = peso/ (altura *altura);
 
-var imc = peso/ (altura *altura);
+  tdImc.textContent = imc.toFixed(2);
 
-tdImc.textContent = imc;
-
+  }
 }
